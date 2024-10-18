75cbd7ca180ed4327598cd53713f2be939e5e1c3
EijiroNotME <jusphervergara09@gmail.com>
2024-10-18 10:01:51 +0800

    few-changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { ProfileProvider } from "../src/custom-hooks/useProfileContext";
+
 import Dashboard from "./pages/Dashboard";
 import "./App.css";
 
 const App = () => {
   return (
-    <ProfileProvider>
-      <div className="mx-auto my-16 md:my-0 grid place-items-center h-full md:h-screen w-11/12 md:max-w-4xl">
-        <Dashboard />
-      </div>
-    </ProfileProvider>
+    <div className="mx-auto my-16 md:my-0 grid place-items-center h-full md:h-screen w-11/12 md:max-w-4xl">
+      <Dashboard />
+    </div>
   );
 };
 
